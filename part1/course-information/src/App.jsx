e29f49875d96c5a46c4af4eebb1a8291f9117889
juan312 <juanalbani48@gmail.com>
2024-12-30 13:36:31 -0300

    Exercise 1.3 - Modified variable definitions to use objects instead

--- part1/course-information/src/App.jsx
+++ part1/course-information/src/App.jsx
@@ -42,22 +42,28 @@ const Total = (props) => {
 
 const App = () => {
 	const course = "Half Stack application development";
-	const part1 = "Fundamentals of React";
-	const exercises1 = 10;
-	const part2 = "Using props to pass data";
-	const exercises2 = 7;
-	const part3 = "State of a component";
-	const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10
+  }
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7
+  }
+  const part3 = {
+    name: "State of a component",
+    exercises: 14
+  }
 
 	return (
 		<div>
 			<Header course={course} />
 			<Content
-				part1={{ name: part1, exercises: exercises1 }}
-				part2={{ name: part2, exercises: exercises2 }}
-				part3={{ name: part3, exercises: exercises3 }}
+				part1={part1}
+				part2={part2}
+				part3={part3}
 			/>
-			<Total exercises={[exercises1, exercises2, exercises3]} />
+			<Total exercises={[part1.exercises, part2.exercises, part3.exercises]} />
 		</div>
 	);
 };
