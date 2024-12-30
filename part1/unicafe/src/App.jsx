f7e6521a1dd73d07ff8baea443d88e3e70a30d50
juan312 <juanalbani48@gmail.com>
2024-12-30 18:33:40 -0300

    Exercise 1.7 - Created statistics component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,32 +1,43 @@
-import { useState } from 'react'
+import { useState } from "react";
+
+const Statistics = ({feedback}) => {
+  const {good, bad, neutral} = feedback;
+	const totalFeedback = good + bad + neutral;
+	const average = (good - bad) / totalFeedback;
+	const percentagePositive = (good / totalFeedback) * 100;
+
+	return (
+		<>
+			<p>Total {totalFeedback}</p>
+			<p>Average {average}</p>
+			<p>Positive {percentagePositive}%</p>
+		</>
+	);
+};
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+	// save clicks of each button to its own state
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
 
-  const totalFeedback = good + bad + neutral;
-  const average = (good - bad) / totalFeedback;
-  const percentagePositive = good / totalFeedback * 100;
+	const constructHandler = (value, setValue) => () => {
+		setValue(value + 1);
+	};
 
-  const constructHandler = (value, setValue) => () => {setValue(value + 1)}
+	return (
+		<div>
+			<h1>Give feedback!</h1>
+			<button onClick={constructHandler(good, setGood)}>good</button>
+			<button onClick={constructHandler(bad, setBad)}>bad</button>
+			<button onClick={constructHandler(neutral, setNeutral)}>neutral</button>
+			<h1>Statistics</h1>
+			<p>Good {good}</p>
+			<p>Neutral {neutral}</p>
+			<p>Bad {bad}</p>
+      <Statistics feedback={{good, bad, neutral}}/>
+		</div>
+	);
+};
 
-  return (
-    <div>
-      <h1>Give feedback!</h1>
-      <button onClick={constructHandler(good, setGood)}>good</button>
-      <button onClick={constructHandler(bad, setBad)}>bad</button>
-      <button onClick={constructHandler(neutral, setNeutral)}>neutral</button>
-      <h1>Statistics</h1>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>Total {totalFeedback}</p>
-      <p>Average {average}</p>
-      <p>Total {percentagePositive}%</p>
-    </div>
-  )
-}
-
-export default App
+export default App;
