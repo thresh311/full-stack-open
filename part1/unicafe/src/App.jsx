7759bfebfba780b632b8ceb659d3bb03d04a509d
juan312 <juanalbani48@gmail.com>
2024-12-30 18:08:04 -0300

    Exercise 1.6: Add functionality for collecting feedback from students

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,9 +6,18 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const constructHandler = (value, setValue) => () => {setValue(value + 1)}
+
   return (
     <div>
-      code here
+      <h1>Give feedback!</h1>
+      <button onClick={constructHandler(good, setGood)}>good</button>
+      <button onClick={constructHandler(bad, setBad)}>bad</button>
+      <button onClick={constructHandler(neutral, setNeutral)}>neutral</button>
+      <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
     </div>
   )
 }
