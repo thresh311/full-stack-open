dad981847da48cf0a51b68ce756b0659502b1501
juan312 <juanalbani48@gmail.com>
2024-12-31 00:29:11 -0300

    Exercise 2.4: Allow the rendering of multiple courses

--- part2/course-information/src/App.jsx
+++ part2/course-information/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from "/vite.svg";
 const Header = ({ name }) => {
 	return (
 		<>
-			<h1>{name}</h1>
+			<h2>{name}</h2>
 		</>
 	);
 };
@@ -35,43 +35,65 @@ const Total = ({ parts }) => {
 	}, 0);
 	return (
 		<>
-			<p><b>Total of {total} exercises</b></p>
+			<p>
+				<b>Total of {total} exercises</b>
+			</p>
 		</>
 	);
 };
 
 const App = () => {
-	const course = {
-		id: 1,
-		name: "Half Stack application development",
-		parts: [
-			{
-				name: "Fundamentals of React",
-				exercises: 10,
-				id: 1,
-			},
-			{
-				name: "Using props to pass data",
-				exercises: 7,
-				id: 2,
-			},
-			{
-				name: "State of a component",
-				exercises: 14,
-				id: 3,
-			},
-			{
-				name: "Crazyness",
-				exercises: 14,
-				id: 4,
-			},
-		],
-	};
+	const courses = [
+		{
+			name: "Half Stack application development",
+			id: 1,
+			parts: [
+				{
+					name: "Fundamentals of React",
+					exercises: 10,
+					id: 1,
+				},
+				{
+					name: "Using props to pass data",
+					exercises: 7,
+					id: 2,
+				},
+				{
+					name: "State of a component",
+					exercises: 14,
+					id: 3,
+				},
+				{
+					name: "Redux",
+					exercises: 11,
+					id: 4,
+				},
+			],
+		},
+		{
+			name: "Node.js",
+			id: 2,
+			parts: [
+				{
+					name: "Routing",
+					exercises: 3,
+					id: 1,
+				},
+				{
+					name: "Middlewares",
+					exercises: 7,
+					id: 2,
+				},
+			],
+		},
+	];
 
 	return (
 		<div>
-			<Course course={course} />
-			<Total parts={course.parts} />
+			<h1>Web Development curriculum</h1>
+			{courses.map((c) => {
+				return <Course key={c.id} course={c} />;
+			})}
 		</div>
 	);
 };
@@ -81,6 +103,7 @@ const Course = ({ course }) => {
 		<div>
 			<Header name={course.name} />
 			<Content parts={course.parts} />
+			<Total parts={course.parts} />
 		</div>
 	);
 };
