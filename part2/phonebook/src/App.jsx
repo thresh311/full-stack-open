68f006111628bf90b89aab6b61906a4262e3b6a3
juan312 <juanalbani48@gmail.com>
2024-12-31 18:04:46 -0300

    Exercise 2.11 - Fetch inital list of people from server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Filter = ({ nameFilter, setNameFilter }) => {
 	return (
@@ -57,14 +58,14 @@ const Person = ({ person }) => {
 };
 
 const App = () => {
-	const [people, setPeople] = useState([
-		{ name: "Arto Hellas", number: "040-123456", id: 1 },
-		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
-		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-	]);
+	const [people, setPeople] = useState([]);
 	const [newPerson, setNewPerson] = useState({ name: "", number: "" });
 	const [nameFilter, setNameFilter] = useState("");
+	useEffect(() => {
+		axios.get("http://localhost:3001/persons").then((response) => {
+			setPeople(response.data);
+		});
+	}, []);
 	const handleOnSumbmit = (e) => {
 		e.preventDefault();
 		if (people.some((p) => p.name === newPerson.name)) {
